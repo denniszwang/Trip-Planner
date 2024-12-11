10255d1de45c6eadf92323dda76a7beec0881e50
DENNIS WANG <denniswz123@gmail.com>
2024-12-11 00:15:46 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { indigo, amber } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
 
-function App() {
+import Search from "./pages/Search";
+
+export const theme = createTheme({
+  palette: {
+    primary: indigo,
+    secondary: amber,
+  },
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import "./Search.css";
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  Autocomplete,
+} from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "50%",
+  left: "25%",
+  height: "250px",
+};
+
+const weatherApi = "aa35184e47a34fac778f4b7ebb54344f";
+const mapApi = "AIzaSyAAkxzWh-FQW3UkJjQPzonay6kGyEC86Wg";
+const philly = {
+  lat: 39.9526,
+  lng: -75.1652,
+};
+
+const SearchCity = () => {
+  const [location, setLocation] = useState(philly);
+  const [autocomplete, setAutocomplete] = useState(null);
+  const [weather, setWeather] = useState(null);
+
+  const onLoad = (autocompleteInstance) => {
+    setAutocomplete(autocompleteInstance);
+  };
+
+  const onPlaceChanged = () => {
+    if (autocomplete !== null) {
+      const place = autocomplete.getPlace();
+      if (place.geometry) {
+        const { lat, lng } = place.geometry.location;
+        setLocation({
+          lat: lat(),
+          lng: lng(),
+        });
+        const city = place.address_components[0].long_name;
+        fetchWeather(city).then((data) => setWeather(data));
+      } else {
+        console.error("No geometry information for selected place.");
+      }
+    } else {
+      console.error("Autocomplete is not loaded yet!");
+    }
+  };
+
+  const fetchWeather = async (city) => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApi}&units=imperial`
+    );
+    const data = await response.json();
+    return {
+      city: data.name,
+      temperature: data.main.temp,
+      humidity: data.main.humidity,
+      wind: data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
+    };
+  };
+
+  useEffect(() => {
+    const fetchDefaultWeather = async () => {
+      setWeather(await fetchWeather("Philadelphia"));
+    };
+    fetchDefaultWeather();
+  }, []);
+
+  return (
+    <div>
+      <LoadScript googleMapsApiKey={mapApi} libraries={["places"]}>
+        <Autocomplete
+          onLoad={onLoad}
+          onPlaceChanged={onPlaceChanged}
+          className="search-container"
+        >
+          <input
+            type="text"
+            placeholder="Philadelphia"
+            className="search-input"
+          />
+        </Autocomplete>
+        {weather && (
+          <div className="weather-container">
+            <img
+              src={weather.icon}
+              alt="weather icon"
+              className="weather-icon"
+            />
+            <h1 className="weather-temp">{weather.temperature}°F</h1>
+            <h2 className="weather-city">{weather.city}</h2>
+            <div className="weather-detail">
+              <div className="detail-item">
+                <p>
+                  <span>Humidity:</span> {weather.humidity}%
+                </p>
+              </div>
+              <div className="detail-item">
+                <p>
+                  <span>Wind Speed:</span> {weather.wind} mph
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={location}
+          zoom={12}
+        >
+          <Marker position={location} />
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
+};
+
+export default SearchCity;
